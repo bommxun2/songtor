36ea514921ed05f75bf24d74b3a15de398391065
Bommxun2 <wachirakorn.cn@gmail.com>
2026-03-11 03:04:25 +0700

    Change api gateway from http -> rest api

--- deployments/opentofu/apigw.tf
+++ deployments/opentofu/apigw.tf
@@ -1,40 +1,60 @@
 # Create HTTP API Gateway
-resource "aws_apigatewayv2_api" "go_app_api" {
+resource "aws_api_gateway_rest_api" "go_app_api" {
   name          = "songtor-http-api"
-  protocol_type = "HTTP"
   description   = "API Gateway routing traffic to Go App on EC2"
 }
 
-# Create Integration
-resource "aws_apigatewayv2_integration" "ec2_integration" {
-  api_id             = aws_apigatewayv2_api.go_app_api.id
-  integration_type   = "HTTP_PROXY"
-  
-  # Pointing to EC2 instance's public IP and port 8080 where Go App is running
-  integration_uri    = "http://${aws_instance.app_server.public_ip}:8080"
-  
-  # HTTp method
-  integration_method = "POST" 
+# Create Resource for /v1
+resource "aws_api_gateway_resource" "v1" {
+  rest_api_id = aws_api_gateway_rest_api.go_app_api.id
+  parent_id   = aws_api_gateway_rest_api.go_app_api.root_resource_id
+  path_part   = "v1"
 }
 
 # Create Route
-resource "aws_apigatewayv2_route" "post_notification_route" {
-  api_id    = aws_apigatewayv2_api.go_app_api.id
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.go_app_api.id
+  parent_id   = aws_api_gateway_resource.v1.id
+  path_part   = "{proxy+}" 
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.go_app_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "ec2_proxy" {
+  rest_api_id             = aws_api_gateway_rest_api.go_app_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
   
-  # Define the route key (HTTP method + path) and link it to the integration
-  route_key = "POST /v1/notifications"
-  target    = "integrations/${aws_apigatewayv2_integration.ec2_integration.id}"
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  
+  uri = "http://${aws_instance.app_server.public_ip}:8080/v1/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
 }
 
-# Create Stage
-resource "aws_apigatewayv2_stage" "default_stage" {
-  api_id      = aws_apigatewayv2_api.go_app_api.id
-  name        = "$default"
-  auto_deploy = true
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.ec2_proxy]
+  rest_api_id = aws_api_gateway_rest_api.go_app_api.id
 }
 
-# Output the API endpoint URL
-output "api_endpoint_post_notification" {
-  description = "URL for POST /v1/notifications"
-  value       = "${aws_apigatewayv2_stage.default_stage.invoke_url}v1/notifications"
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.go_app_api.id
+  stage_name    = "prod"
+}
+
+output "rest_api_url" {
+  value = "${aws_api_gateway_stage.prod.invoke_url}"
 }
