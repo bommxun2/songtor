4f07dd976cfb9a8c625ded4618bc316b998c4bba
Bommxun2 <wachirakorn.cn@gmail.com>
2026-04-22 15:51:09 +0700

    add port 80

--- deployments/terraform/modules/security/main.tf
+++ deployments/terraform/modules/security/main.tf
@@ -50,6 +50,13 @@ resource "aws_security_group" "ecs" {
   vpc_id = aws_vpc.vpc.id
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = var.container_port
     to_port     = var.container_port
     protocol    = "tcp"
