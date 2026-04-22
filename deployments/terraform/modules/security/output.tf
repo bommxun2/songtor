318b0913840e7e5ad006d37478998d9efd448d49
Bommxun2 <wachirakorn.cn@gmail.com>
2026-04-22 15:06:51 +0700

    modify output

--- deployments/terraform/modules/security/output.tf
+++ deployments/terraform/modules/security/output.tf
@@ -1,5 +1,13 @@
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
 output "subnet_id" {
   value = aws_subnet.public_subnet_1.id
+}
+
+output "subnet_ids" {
+  value = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 }
 
 output "db_subnet_group_name" {
