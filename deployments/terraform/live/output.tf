0c62b5bcaa175797362edc8acd00993543230570
Bommxun2 <wachirakorn.cn@gmail.com>
2026-05-17 15:32:19 +0700

    update output.tf

--- deployments/terraform/live/output.tf
+++ deployments/terraform/live/output.tf
@@ -2,8 +2,12 @@ output "rds_endpoint" {
   value = module.rds.rds_endpoint
 }
 
-output "sns_topic_arn" {
-  value = module.sns.sns_topic_arn
+output "sns_patient_reported_topic_arn" {
+  value = module.patient_reported_sns.sns_topic_arn
+}
+
+output "sns_critical_case_topic_arn" {
+  value = module.critical_case_sns.sns_topic_arn
 }
 
 output "api_gateway_endpoint" {
